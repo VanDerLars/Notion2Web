dcf4d8316bceae9367a7a771c78a70037376b2af
Lars L <lars@lehmann.link>
2022-02-20 16:01:43 +0100

    .

--- config.php
+++ config.php
@@ -1,44 +1,45 @@
 <?php
-// Declaration; 
-$use_image_light_boxes;
-global $open_external_links_in_new_browser_tab;
 
 // general settings
-$language = 'en';
-$page_title = 'Notion2Web';
+$language = 'en'; // gives the clients browser the information about the used language
+$page_title = ''; //displayed in the main menu (depends on the theme). Empty as standard, since the logo tells the name
+$seo_title = 'Notion2Web - the Notion website builder'; // displayed in google and in the browser tab
+$seo_description = 'Notion2Web turns your Notion HTML export into a fully functional static website in no time.';
+$seo_robots = 'index, follow';
 
 // display settings
-$template = 'sidebar_light'; // the template name which should be used to render the page. You can define multiple
+$theme = 'sidebar_light'; // the theme name which should be used to render the page.
+// $theme = 'sidebar_light_full_width'; 
+// $theme = 'sidebar_dark'; 
+// $theme = 'sidebar_dark_full_width';
 
+// menu settings
+$main_logo_light = 'img/logo_light.png';
+$main_logo_dark = 'img/logo_dark.png';
 
-// main menu settings
-$main_logo_light = 'img/VIA_plain_blau S.png';
-$main_logo_dark = '';
-
-// $menu = [$link1: "url1",$link2: "url2"];
 $menu_left = [
 	'Blog ↗' => "https://blog.stadtlandnetz.de",
 ];
 $menu_right = [
-	'Support ↗' => "http://localhost:3000/localhost?path=content&name=Wiki+3084aced05f749129552978d659ed9bc.html&id=3084aced05f749129552978d659ed9bc",
+	'internal Item' => "/localhost?path=content%2FNotion2Web+373b5&name=02+The+N2W+0131e.html&id=0131e",
 	'Stadt.Land.Netz ↗' => "https://stadtlandnetz.de",
-	'Third Item ↗' => "https://google.com",
 ];
 
 $footer_menu_left = [
-	'Impressum ↗' => "http://localhost:3000/localhost?path=content&name=Wiki+3084aced05f749129552978d659ed9bc.html&id=3084aced05f749129552978d659ed9bc",
-	'Datenschutz ↗' => "https://stadtlandnetz.de",
+	'Imprint ↗' => "https://an-url.com",
+	'Data privacy ↗' => "https://another-url.com",
 ];
 $footer_menu_right = [
 	'↑' => "#top",
 ];
 
 
+// END OF CHANGEABLE THINGS
+
 // set up templating engine
-$smarty->setTemplateDir('app/templates');
+$smarty->setTemplateDir('app/themes');
 $smarty->setConfigDir('app/smarty/config');
 $smarty->setCompileDir('app/smarty/compile');
 $smarty->setCacheDir('app/smarty/cache');
-// $smarty->testInstall();
 
 ?>
